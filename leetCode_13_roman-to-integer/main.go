a1a9fdf11f00066892378634a26b5ab5a423e909
maqingjie <maqingjie@rockontrol.com>
2023-04-04 17:30:39 +0800

    update

---
+++ leetCode_13_roman-to-integer/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	I     = 1
+	V     = 5
+	X     = 10
+	L     = 50
+	C     = 100
+	D     = 500
+	M     = 1000
+	roman = "III"
+	arr   = []int{}
+)
+
+func main() {
+	//fmt.Println(len(roman))
+	for i := 0; i < len(roman); i++ {
+		fmt.Println(roman[i])
+
+	}
+}
+func roman_number(a string) {
+
+	switch a {
+	//case buffer.Buffer(a) == "I":
+	//return
+
+	}
+
+}
